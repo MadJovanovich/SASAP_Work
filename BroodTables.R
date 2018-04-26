4b065d6786f4a01b2b2d7338363f8aeabd7fc008
Madeline Jovanovich <mjovanovich@alaska.edu>
2018-04-26 12:34:43 -0800

    Committing progress, still lots to do.

---
+++ BroodTables.R
@@ -0,0 +1,18 @@
+#Compiling brood tables for WDAFS Talk: Case Studies
+#Some of these KNB sites still seem pretty cryptic... what's up with that? 
+
+Chinook_NEPac <- read.csv("https://knb.ecoinformatics.org/knb/d1/mn/v2/object/urn%3Auuid%3A2dd0cf28-faf1-4b16-ab31-28c792e75d92")
+KuskoCoho <- read.csv("https://knb.ecoinformatics.org/knb/d1/mn/v2/object/knb.92221.1")
+Goodnews_Sockeye <- read.csv("https://knb.ecoinformatics.org/knb/d1/mn/v2/object/knb.92143.1")
+Goodnews_Chinook <- read.csv("https://knb.ecoinformatics.org/knb/d1/mn/v2/object/knb.92146.1")
+
+Sockeye_NEPac <- read.csv("https://knb.ecoinformatics.org/knb/d1/mn/v2/object/urn%3Auuid%3Ac747eee7-89fd-44ba-bce1-a14d0670792d")
+Nak <- read.csv("https://knb.ecoinformatics.org/knb/d1/mn/v2/object/urn%3Auuid%3A0c29694f-0b38-4de1-a91d-14ab9b9c7e1d")
+Ege <- read.csv("https://knb.ecoinformatics.org/knb/d1/mn/v2/object/urn%3Auuid%3Ad0fe0463-bc3a-4d7c-94a4-c6f78a9cd3d9")
+Kvi <- read.csv("https://knb.ecoinformatics.org/knb/d1/mn/v2/object/urn%3Auuid%3Ac45c74ce-9795-4c1c-85df-4ed0c95c6880")
+Uga <- read.csv("https://knb.ecoinformatics.org/knb/d1/mn/v2/object/urn%3Auuid%3Adb3bd609-6f14-4790-ad93-48f56b5e3602")
+Ala <- read.csv("https://knb.ecoinformatics.org/knb/d1/mn/v2/object/urn%3Auuid%3A3fcedce7-38ea-4442-ae7e-a952da330beb")
+Tog <- read.csv("https://knb.ecoinformatics.org/knb/d1/mn/v2/object/urn%3Auuid%3Ae758912c-057c-4987-ae64-1777202152d8")
+Wood <- read.csv("https://knb.ecoinformatics.org/knb/d1/mn/v2/object/urn%3Auuid%3A87f4bc06-5fed-4391-b6a4-1e1a8dfaefb4")
+Nush <- read.csv("https://knb.ecoinformatics.org/knb/d1/mn/v2/object/urn%3Auuid%3A329bb96e-6a4c-4170-a933-87f53d67343d")
+Igu <- read.csv("https://knb.ecoinformatics.org/knb/d1/mn/v2/object/urn%3Auuid%3A514f65fa-7f6b-4276-b502-4f46834d309b")
